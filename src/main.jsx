a2a6c50128fc17b93a9da0899c3a981a67182189
SK Maqshood <skmaqshood99@gmail.com>
2026-05-30 15:39:56 +0530

    add ThemeContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ThemeProvider } from "./themecontext.jsx";
+import { ThemeProvider } from "./ThemeContext.jsx";
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <App />
